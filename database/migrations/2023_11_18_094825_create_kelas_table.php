abb90feb5c0ce423f13b01b304708cbfcdaf1409
DAVIN WAHYU WARDANA <davinww060304@gmail.com>
2023-11-18 16:51:19 +0700

    Tabel Kelas

---
+++ database/migrations/2023_11_18_094825_create_kelas_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('kelas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama_kelas');
+            $table->bigInteger('id_guru')->unsigned();
+            $table->foreign('id_guru')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('kelas');
+    }
+};
