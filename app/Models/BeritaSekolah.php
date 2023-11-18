7d659616ffca80d43b5c3b0c6b3859dea33f9fed
DAVIN WAHYU WARDANA <davinww060304@gmail.com>
2023-11-18 21:32:13 +0700

    Model & Controller

---
+++ app/Models/BeritaSekolah.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BeritaSekolah extends Model
+{
+    use HasFactory;
+}
