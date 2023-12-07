37785a8a8c27e02870b911922dfd8560c2dd8883
DAVIN WAHYU WARDANA <davinww060304@gmail.com>
2023-12-07 13:57:06 +0700

    Home Page

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.index');
+});
+Route::get('/home', function () {
+    return view('home.index');
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
