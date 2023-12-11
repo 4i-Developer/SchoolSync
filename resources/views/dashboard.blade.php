30061881fc5e066c50b15ddc7f772f276d4bdc11
DAVIN WAHYU WARDANA <davinww060304@gmail.com>
2023-12-11 13:24:38 +0700

    Fix Image Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -43,7 +43,7 @@
         @endif
     </x-slot>
 
-    @if(Auth::user()->role === 'admin' || Auth::user()->role === 'wali' || Auth::user()->role === 'guru')
+    @if(Auth::user()->role === 'admin' || Auth::user()->role === 'guru')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <img src="{{ asset('images/dashboard/student.png') }}" alt="Dashboard Image" class="w-full">
